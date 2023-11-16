2c0e7b0387f2d66643a7f3466df3545563cc6460
Jahid Hasan <jahidhasanofficial23@gmail.com>
2023-11-17 01:45:05 +0600

    add assignment

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Support\Facades\Cookie;
+
+
+class ProfileController extends Controller
+{
+    public function index($name)
+    {
+       // $name = "Donal Trump";
+       $id= '4';
+        $age = "75";
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $cookie = Cookie::make('access_token', '123-XYZ', 1, '/', $_SERVER['SERVER_NAME'], false, true);
+        return response()->json($data)->withCookie($cookie)->setStatusCode(200);
+    }
+}
